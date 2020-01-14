9d465dea0f68b9991a1d82b3c1818de541695a94
6abrielMR <gabriel.montes716@gmail.com>
2020-01-13 21:20:40 -0500

    Selector colors and login fake.

--- js/main.js
+++ js/main.js
@@ -59,4 +59,48 @@ $(document).ready(function () {
         $('#posts').append(post);
     });
 
+    //Selector de Tema
+    let theme = $('#theme');
+    $('#to-green').click(function() {
+        theme.attr('href', 'css/green.css');
+    });
+
+    $('#to-red').click(function() {
+        theme.attr('href', 'css/red.css');
+    });
+
+    $('#to-blue').click(function() {
+        theme.attr('href', 'css/blue.css');
+    });
+
+    //Scroll arriba de la web
+
+    $('.up').click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
+    //Login fake
+
+    $('#login form').submit(function() {
+        let form_name = $('#form_name').val();
+        localStorage.setItem('form_name', form_name);
+    });
+
+    let form_name = localStorage.getItem('form_name');
+    if (form_name !== null && form_name !== 'undefined') {
+        let about_paragrhap = $('#about p');
+        $('#about p').html('<br><strong>Bienvenido, ' + form_name) + '</strong>';
+        about_paragrhap.append("<br><br><a href='#' id='logout'>Cerrar Sesión</a>");
+        $('#login').hide();
+
+        $('#logout').click(function() {
+            localStorage.clear();
+            location.reload();
+        });
+    }
+
 });
